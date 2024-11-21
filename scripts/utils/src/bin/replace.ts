84d2f0c05cdd14b27d6741c5a9f065bdb15e8126
“wadv-code” <853925809@qq.com>
2024-11-21 11:09:33 +0800

    replace.ts执行替换时，排除自身的target 字符。

--- scripts/utils/src/bin/replace.ts
+++ scripts/utils/src/bin/replace.ts
@@ -3,13 +3,19 @@ import { startReplace } from '../lib/replace';
 
 (async function () {
   // 排除目录或文件夹
-  const excludes: string[] = ['node_modules', 'dist', '.turbo', 'dist.zip', '/utils/src/bin/*'];
+  const excludes: string[] = [
+    'node_modules',
+    'dist',
+    '.turbo',
+    'dist.zip',
+    '**/utils/src/bin/replace.ts',
+  ];
   // 要修改的文件
   const targets: ReplaceTargets = [
     // 指定文件
     {
       pattern: '*.{vue,js,ts,jsx,tsx,svelte,astro,html}',
-      target: '这是一个关于我们的页面',
+      target: 'This is a about page',
       replace: '这是一个关于我们的页面',
     },
     // 替换成时间
