721fc1eeaaf061cd2185401c6dc9498d9ea052c8
“wadv-code” <853925809@qq.com>
2024-11-22 17:50:55 +0800

    鼠标

--- admin/src/views/HomeView.vue
+++ admin/src/views/HomeView.vue
@@ -48,15 +48,15 @@ function toggleTheme(event: MouseEvent) {
       <Button @click="toggleTheme">切换{{ isDark ? '浅色' : '深色' }}</Button>
       <Calendar v-model="value" weekday-format="short" class="rounded-md border mt-2" />
       <DropdownMenu />
-      <Button @click="toggleTheme" class="">动画跟随按钮</Button>
+      <Button @click="toggleTheme" class="">动画跟随鼠标</Button>
       <div class="flex items-center space-x-2 mt-5">
         <Switch id="airplane-mode" />
         <Label for="airplane-mode">Airplane Mode</Label>
       </div>
     </div>
-    <Button @click="toggleTheme" class="absolute left-2 top-2">动画跟随按钮</Button>
-    <Button @click="toggleTheme" class="absolute right-2 top-2">动画跟随按钮</Button>
-    <Button @click="toggleTheme" class="absolute left-2 bottom-2">动画跟随按钮</Button>
-    <Button @click="toggleTheme" class="absolute right-2 bottom-2">动画跟随按钮</Button>
+    <Button @click="toggleTheme" class="absolute left-2 top-2">动画跟随鼠标</Button>
+    <Button @click="toggleTheme" class="absolute right-2 top-2">动画跟随鼠标</Button>
+    <Button @click="toggleTheme" class="absolute left-2 bottom-2">动画跟随鼠标</Button>
+    <Button @click="toggleTheme" class="absolute right-2 bottom-2">动画跟随鼠标</Button>
   </div>
 </template>
